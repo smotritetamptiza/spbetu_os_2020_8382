5b75e2f475a5247e3cb576eff09cd3dd82b64c12
Michael <jakutenshi@gmail.com>
2020-03-01 22:24:52 +0300

    Add fetch scripts

---
+++ fetch.sh
@@ -0,0 +1,16 @@
+# git fetch caxapexac         # Сахаров Виктор
+# git fetch LemonGrace        # Гречко Вероника
+# git fetch Elli-zveg         # Звегинцева Елизавета
+# git fetch garet2gis         # Муковский Данил
+git fetch the21composer     # Облизанов Александр
+# git fetch olesy             # Ивлева Олеся
+# git fetch StavrWalker       # Гоголев Евгений
+# git fetch Fireralin         # Перелыгин Дмитрий
+git fetch Nik-Poch          # Почаев Никита
+# git fetch NguyenHai-LETI    # Нгуен Ши Хай
+# git fetch Dmitriy129        # Переверзев Дмитрий
+# git fetch GandhiKK          # Киреев Константин
+# git fetch milesartemius     # Сергеев Александр
+# git fetch Anstberd          # Бердникова Анастасия
+# git fetch marilisok         # Лисок Мария
+# git fetch DmitriySosnovskiy # Сосновский Дмитрий
---
+++ remotes.sh
@@ -0,0 +1,16 @@
+# git remote add caxapexac         https://github.com/caxapexac/spbetu_os_2020_8383.git         # Сахаров Виктор
+# git remote add LemonGrace        https://github.com/LemonGrace/spbetu_os_2020_8383.git        # Гречко Вероника
+# git remote add Elli-zveg         https://github.com/Elli-zveg/spbetu_os_2020_8383.git         # Звегинцева Елизавета
+# git remote add garet2gis         https://github.com/garet2gis/spbetu_os_2020_8383.git         # Муковский Данил
+git remote add the21composer     https://github.com/the21composer/spbetu_os_2020_8383.git     # Облизанов Александр
+# git remote add olesy             https://github.com/olesy/spbetu_os_2020_8383.git             # Ивлева Олеся
+# git remote add StavrWalker       https://github.com/StavrWalker/spbetu_os_2020_8383.git       # Гоголев Евгений
+# git remote add Fireralin         https://github.com/Fireralin/spbetu_os_2020_8383.git         # Перелыгин Дмитрий
+git remote add Nik-Poch          https://github.com/Nik-Poch/spbetu_os_2020_8383.git          # Почаев Никита
+# git remote add NguyenHai-LETI    https://github.com/NguyenHai-LETI/spbetu_os_2020_8383.git    # Нгуен Ши Хай
+# git remote add Dmitriy129        https://github.com/Dmitriy129/spbetu_os_2020_8383.git        # Переверзев Дмитрий
+# git remote add GandhiKK          https://github.com/GandhiKK/spbetu_os_2020_8383.git          # Киреев Константин
+# git remote add milesartemius     https://github.com/milesartemius/spbetu_os_2020_8383.git     # Сергеев Александр
+# git remote add Anstberd          https://github.com/Anstberd/spbetu_os_2020_8383.git          # Бердникова Анастасия
+# git remote add marilisok         https://github.com/marilisok/spbetu_os_2020_8383.git         # Лисок Мария
+# git remote add DmitriySosnovskiy https://github.com/DmitriySosnovskiy/spbetu_os_2020_8383.git # Сосновский Дмитрий
