4585230a9e2aab9655930b838cf4e3533edb3084
Michael <jakutenshi@gmail.com>
2020-03-05 13:14:49 +0300

    Create remote and fetch scripts

---
+++ fetch.sh
@@ -0,0 +1,21 @@
+git fetch rakhele           # Рочева
+git fetch sinelnikovmaxim   # Синельников
+git fetch snchz29           # Терехов
+git fetch AhimasYD          # Янкин
+# git fetch  #
+# git fetch  #
+# git fetch  #
+# git fetch  #
+# git fetch  #
+# git fetch  #
+# git fetch  #
+# git fetch  #
+# git fetch  #
+# git fetch  #
+# git fetch  #
+# git fetch  #
+# git fetch  #
+# git fetch  #
+# git fetch  #
+# git fetch  #
+# git fetch  #
---
+++ remotes.sh
@@ -0,0 +1,4 @@
+git remote add rakhele         https://github.com/rakhele/spbetu_os_2020_8382.git         # Рочева
+git remote add sinelnikovmaxim https://github.com/sinelnikovmaxim/spbetu_os_2020_8382.git # Синельников
+git remote add snchz29         https://github.com/snchz29/spbetu_os_2020_8382.git         # Терехов
+git remote add AhimasYD        https://github.com/AhimasYD/spbetu_os_2020_8382.git        # Янкин
