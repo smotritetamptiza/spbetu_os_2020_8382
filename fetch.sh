4775c27d230068f44eae740a7ff6eeec8971bb4b
Michael <jakutenshi@gmail.com>
2020-03-29 02:16:17 +0300

    Add kolnogorov to remote-fetch scripts

--- fetch.sh
+++ fetch.sh
@@ -2,7 +2,7 @@ git fetch rakhele           # Рочева
 git fetch sinelnikovmaxim   # Синельников
 git fetch snchz29           # Терехов
 git fetch AhimasYD          # Янкин
-# git fetch  #
+git fetch pajecawa          # Колногоров
 # git fetch  #
 # git fetch  #
 # git fetch  #
--- remotes.sh
+++ remotes.sh
@@ -2,3 +2,4 @@ git remote add rakhele         https://github.com/rakhele/spbetu_os_2020_8382.gi
 git remote add sinelnikovmaxim https://github.com/sinelnikovmaxim/spbetu_os_2020_8382.git # Синельников
 git remote add snchz29         https://github.com/snchz29/spbetu_os_2020_8382.git         # Терехов
 git remote add AhimasYD        https://github.com/AhimasYD/spbetu_os_2020_8382.git        # Янкин
+git remote add pajecawa vhttps://github.com/pajecawav/spbetu_os_2020_8382.git         # Рочева
