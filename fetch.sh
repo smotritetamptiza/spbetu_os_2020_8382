4f2336416f6ba8b2292097b9a874b014c8619207
Michael <jakutenshi@gmail.com>
2020-04-30 23:22:02 +0300

    Add several students to the scripts

--- fetch.sh
+++ fetch.sh
@@ -3,11 +3,11 @@ git fetch sinelnikovmaxim   # Синельников
 git fetch snchz29           # Терехов
 git fetch AhimasYD          # Янкин
 git fetch pajecawa          # Колногоров
-# git fetch  #
-# git fetch  #
-# git fetch  #
-# git fetch  #
-# git fetch  #
+git fetch LangerKrieg       # Наконечная
+git fetch PavelChernitsyn   # Черницын
+git fetch Pavel-by          # Мирончик
+git fetch inoriin           # Чирков
+git fetch ephemerno         # Ефимова
 # git fetch  #
 # git fetch  #
 # git fetch  #
--- remotes.sh
+++ remotes.sh
@@ -2,4 +2,9 @@ git remote add rakhele         https://github.com/rakhele/spbetu_os_2020_8382.gi
 git remote add sinelnikovmaxim https://github.com/sinelnikovmaxim/spbetu_os_2020_8382.git # Синельников
 git remote add snchz29         https://github.com/snchz29/spbetu_os_2020_8382.git         # Терехов
 git remote add AhimasYD        https://github.com/AhimasYD/spbetu_os_2020_8382.git        # Янкин
-git remote add pajecawa https://github.com/pajecawav/spbetu_os_2020_8382.git         # Колногоров
+git remote add pajecawa        https://github.com/pajecawav/spbetu_os_2020_8382.git       # Колногоров
+git remote add LangerKrieg     https://github.com/LangerKrieg/spbetu_os_2020_8382.git     # Наконечная
+git remote add PavelChernitsyn https://github.com/PavelChernitsyn/spbetu_os_2020_8382.git # Черницын
+git remote add Pavel-by        https://github.com/Pavel-by/spbetu_os_2020_8382.git        # Мирончик
+git remote add inoriin         https://github.com/inoriin/spbetu_os_2020_8382.git         # Чирков
+git remote add ephemerno       https://github.com/ephemerno/spbetu_os_2020_8382.git       # Ефимова
